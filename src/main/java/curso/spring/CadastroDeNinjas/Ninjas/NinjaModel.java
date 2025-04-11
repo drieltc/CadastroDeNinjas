629c9b428aa328b3bfb7db6514120d79f72cd129
Adriel Costa <adryeltc2001@gmail.com>
2025-04-11 18:28:58 -0300

    feat: Coloca o Lombok na classe para abstracao

--- src/main/java/curso/spring/CadastroDeNinjas/Ninjas/NinjaModel.java
+++ src/main/java/curso/spring/CadastroDeNinjas/Ninjas/NinjaModel.java
@@ -2,10 +2,16 @@ package curso.spring.CadastroDeNinjas.Ninjas;
 
 import curso.spring.CadastroDeNinjas.Missoes.MissaoModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 //Entity transforma uma classe comum numa entidade do BD
 @Entity
 @Table(name = "td_cadastro_de_ninjas")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class NinjaModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -21,33 +27,6 @@ public class NinjaModel {
     public NinjaModel(String nome, String email, int idade) {
         this.nome = nome;
         this.email = email;
-        this.idade = idade;
-    }
-    public NinjaModel(){
-
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
         this.idade = idade;
     }
 }
