ece3233ca03e35c012f047a69723a76db728c1d3
Adriel Costa <adryeltc2001@gmail.com>
2025-04-11 17:08:20 -0300

    Criado a Controller

---
+++ src/main/java/curso/spring/CadastroDeNinjas/Controller.java
@@ -0,0 +1,15 @@
+package curso.spring.CadastroDeNinjas;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class Controller {
+
+    @GetMapping("/boasVindas")
+    public String boasVindas(){
+        return "Esta é minha primeira mensagem nesta rota";
+    }
+}
