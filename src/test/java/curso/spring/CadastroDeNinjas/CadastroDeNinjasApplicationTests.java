6531151f4040138b459f514193335c8ed078f396
Adriel Costa <adryeltc2001@gmail.com>
2025-04-11 16:49:41 -0300

    first commit

---
+++ src/test/java/curso/spring/CadastroDeNinjas/CadastroDeNinjasApplicationTests.java
@@ -0,0 +1,13 @@
+package curso.spring.CadastroDeNinjas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroDeNinjasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
